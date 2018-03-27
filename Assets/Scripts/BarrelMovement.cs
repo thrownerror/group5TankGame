faa0bb9e384e7af98a50aead74508895c819636b
Robert Bailey <emailrobertb@gmail.com>
2018-03-26 22:34:53 -0400

    Targets and trigger

--- Assets/Scripts/BarrelMovement.cs
+++ Assets/Scripts/BarrelMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BarrelMovement : MonoBehaviour {
 
 	Camera cam;
+    bool firing;
 	// Use this for initialization
 
 	[SerializeField]
@@ -19,13 +20,17 @@ public class BarrelMovement : MonoBehaviour {
 	//
 	//float fireAngle;
 	public float firePower = 1000f;
+    float powerLevel;
 	GameObject reticle;
-
+    bool powerUp;
+    Rect powerRect;
 	void Start () {
 		cam = Camera.main;
 		//fireAngle = 45.0f;
 		//firePower = minFirePower;
 		reticle = GameObject.FindGameObjectWithTag("reticle");
+        firing = false;
+        powerUp = true;
 	}
 
 	// Update is called once per frame
@@ -49,11 +54,25 @@ public class BarrelMovement : MonoBehaviour {
 			}
 		}*/
 
-		if (Input.GetKeyDown (KeyCode.Space)) 
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetAxisRaw("Fire") != 0.0f) 
 		{
-			Vector3 pos =new Vector3(reticle.GetComponent<SocketClient>().xPos, reticle.GetComponent<SocketClient>().yPos, 0);
-			Fire (pos);
+            if (!firing)
+            {
+                Vector3 pos = new Vector3(reticle.GetComponent<SocketClient>().xPos, reticle.GetComponent<SocketClient>().yPos, 0);
+                firing = true;
+            }
+            else
+            {
+                Fire(mousePos);
+                firing = false;
+                Debug.Log("Power shot" + powerLevel);
+            }
 		}
+        if (firing)
+        {
+            PowerStage();
+            Debug.Log("Powering");
+        }
 	}
 
 	void Fire(Vector3 mPos)
@@ -65,7 +84,7 @@ public class BarrelMovement : MonoBehaviour {
 		print ("x" + dir.x + "y" + dir.y);
 		Vector2 force = new Vector2(dir.x,dir.y) * firePower;
 		//print ("x" + force.x + "y" + force.y);
-		_bulletholder.GetComponent<Rigidbody2D> ().AddRelativeForce (force, ForceMode2D.Impulse);
+		_bulletholder.GetComponent<Rigidbody2D> ().AddRelativeForce (force * powerLevel, ForceMode2D.Impulse);
 
 		//Vector2 dir = (Vector2)(Quaternion.Euler(0, 0, fireAngle) * Vector2.right);
 		//Debug.Log(dir);
@@ -76,5 +95,27 @@ public class BarrelMovement : MonoBehaviour {
 
 		//firePower = minFirePower;
 	}
-
+    void PowerStage()
+    {
+        if (powerUp)
+        {
+            powerLevel = powerLevel + 1.5f * Time.deltaTime;
+            //Debug.Log(barrel.transform.rotation.z);
+            Debug.Log("power " + powerLevel);
+            if (powerLevel > 10) { powerUp = false; }
+        }
+        else if (!powerUp)
+        {
+            powerLevel = powerLevel - 1.5f * Time.deltaTime;
+            //arrel.transform.Rotate(-Vector3.forward * 10 * Time.deltaTime);
+            Debug.Log("power down " + powerLevel);
+            if (powerLevel < 0) { powerUp = true; }//if (barrel.transform.rotation.z <= 0) { powerUp = true; }
+        }
+        powerRect = new Rect(30, 30, 10 * powerLevel + 5, 40);
+    }
+    private void OnGUI()
+    {
+        GUI.color = Color.red;
+        GUI.Box(powerRect, "Power level");
+    }
 }
