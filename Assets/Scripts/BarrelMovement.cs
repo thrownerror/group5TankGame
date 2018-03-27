57a5718e45238b68fd732c93408cb1586ab293a8
kunalpatil95 <kunalpatil95@gmail.com>
2018-03-26 21:15:07 -0400

    projectile

---
+++ Assets/Scripts/BarrelMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelMovement : MonoBehaviour {
+
+	Camera cam;
+	// Use this for initialization
+
+	[SerializeField]
+	GameObject Bullet;
+
+	[SerializeField]
+	Transform shootingPoint;
+
+	// force to fire at
+	//public float fireForce;
+
+	//
+	//float fireAngle;
+	public float firePower = 1000f;
+
+
+	void Start () {
+		cam = Camera.main;
+		//fireAngle = 45.0f;
+		//firePower = minFirePower;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos;
+		mousePos = cam.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - transform.position.z));
+		transform.parent.rotation = Quaternion.LookRotation (Vector3.forward, mousePos - transform.position);
+
+
+
+	/*	if (Input.GetKey(KeyCode.Space))
+		{
+			if (firePower < 1.0f)
+			{
+				firePower += 0.01f;
+				//Debug.Log(firePower);
+			}
+			else
+			{
+				firePower = 1.0f;
+			}
+		}*/
+
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Fire (mousePos);
+		}
+	}
+
+	void Fire(Vector3 mPos)
+	{
+		GameObject _bulletholder = Instantiate (Bullet) as GameObject;
+		_bulletholder.transform.position = shootingPoint.transform.position;
+		_bulletholder.transform.rotation = shootingPoint.transform.rotation;
+		Vector3 dir = Vector3.Normalize( mPos - transform.position);
+		print ("x" + dir.x + "y" + dir.y);
+		Vector2 force = new Vector2(dir.x,dir.y) * firePower;
+		//print ("x" + force.x + "y" + force.y);
+		_bulletholder.GetComponent<Rigidbody2D> ().AddRelativeForce (force, ForceMode2D.Impulse);
+
+		//Vector2 dir = (Vector2)(Quaternion.Euler(0, 0, fireAngle) * Vector2.right);
+		//Debug.Log(dir);
+
+		//Vector2 force = dir * (firePower * fireForce);
+
+		//_bulletholder.GetComponent<Rigidbody2D>().AddForce(force,ForceMode2D.Impulse);
+
+		//firePower = minFirePower;
+	}
+
+}
